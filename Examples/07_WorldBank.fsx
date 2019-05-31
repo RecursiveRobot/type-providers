5eb6ad22e033c34cb428a2cf5d46c2e909522594
Attie Naude <anaude@digiquill.co.za>
2019-05-31 08:03:30 +0200

    Adds World Bank example

---
+++ Examples/07_WorldBank.fsx
@@ -0,0 +1,25 @@
+#r @".\packages\FSharp.Data\lib\net45\FSharp.Data.dll"
+#load @".\packages\FSharp.Charting\FSharp.Charting.fsx"
+
+open FSharp.Data
+open FSharp.Charting
+
+let worldBank = WorldBankData.GetDataContext()
+
+// worldBank.Countries.``South Africa``.Indicators.``Incidence of malaria (per 1,000 population at risk)``
+// |> Seq.toList
+// |> Chart.Line
+
+// let countries = [
+//     worldBank.Countries.``South Africa``;
+//     worldBank.Countries.China;
+//     worldBank.Countries.``United States``
+// ]
+
+// let plotCountry (country : WorldBankData.ServiceTypes.Country) =
+//     Chart.Line (country.Indicators.``CO2 emissions (kt)`` , Name = country.Name) 
+//     |> Chart.WithLegend (true)
+
+// countries
+// |> List.map plotCountry
+// |> Chart.Combine
